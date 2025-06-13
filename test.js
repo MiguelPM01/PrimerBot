e6609757e46b6b51cdbc1f6757765ab2e1d2ec36
Miguel Angel Peralta Martinez <106187870+MiguelPM01@users.noreply.github.com>
2025-06-13 10:18:36 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // whatsapp-test.js
 const token = "EAARuNB2RmpYBOx8DBxCxzc3MEkA6v7oLNLubUMxZB7P0c8oL1X2JtaClbBYDs4eIRA5Ym9T1x5BFJh5DGjZCrQAZBjZA9KIWAZAjn1fl0RcpTNJUMwHI2cAfN9E2ZBz5weoiZBXsekRyRKmRpqWY5pH7MawYH3WwvFxf5uubp8i3oPXasEsy1Xps1cDTkFbICFvWeSrpWdbWaPaIkpvthupqPtMKKL8ieUZD"
+const phone_number_id = "655218471013271";
 const recipient = "525530103559";
 
 const url = `https://graph.facebook.com/v22.0/655218471013271/messages`;
